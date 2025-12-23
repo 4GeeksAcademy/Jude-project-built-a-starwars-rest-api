7bbfad858b5fd6470fe361e62051baab81105c52
cinjude <110071132+cinjude@users.noreply.github.com>
2025-12-23 22:45:08 +0000

    [database]

--- src/admin.py
+++ src/admin.py
@@ -1,16 +1,73 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Character, Planet, Vehicle, Starship, CharacterFavorite, PlanetFavorite, VehicleFavorite, StarshipFavorite
 from flask_admin.contrib.sqla import ModelView
+
+
+class UserModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'email', 'password', 'is_active', 'favorites',
+                   'favorites_planet', 'favorite_vehicle', 'favorite_starship']
+
+
+class CharacterModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'name', 'height', 'gender',
+                   'eyes_color', 'birth_day', 'mass', 'favorites_by']
+
+
+class PlanetModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'name', 'climate',
+                   'diametre', 'population', 'favorite_by']
+
+
+class VehicleModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'name', 'model', 'passengers', 'favorite_by']
+
+
+class StarshipModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'name', 'model',
+                   'passengers', 'length', 'favorite_by']
+
+
+class CharacterFavoriteModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'user_id', 'users', 'character_id', 'character']
+
+
+class PlanetFavoriteModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'user_id', 'users', 'planet_id', 'planetas']
+
+
+class VehicleFavoriteModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'user_id', 'users', 'vehicle_id', 'vehiculo']
+
+
+class StarshipFavoriteModelView(ModelView):
+    column_auto_select_related = True
+    column_list = ['id', 'user_id', 'users', 'starship_id', 'starship']
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(UserModelView(User, db.session))
+    admin.add_view(CharacterModelView(Character, db.session))
+    admin.add_view(PlanetModelView(Planet, db.session))
+    admin.add_view(VehicleModelView(Vehicle, db.session))
+    admin.add_view(StarshipModelView(Starship, db.session))
+    admin.add_view(CharacterFavoriteModelView(CharacterFavorite, db.session))
+    admin.add_view(PlanetFavoriteModelView(PlanetFavorite, db.session))
+    admin.add_view(VehicleFavoriteModelView(VehicleFavorite, db.session))
+    admin.add_view(StarshipFavoriteModelView(StarshipFavorite, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -6,19 +6,35 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__= 'usuario'
+    __tablename__ = 'usuario'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-    favorites: Mapped[list['CharactereFavorite']]= relationship(back_populates='users')
-    favorites_planet: Mapped[list['PlanetFavorite']] = relationship(back_populates = 'users')
-    favorite_vehicle: Mapped[list['VehicleFavorite']] = relationship(back_populates = 'users')
+    favorites: Mapped[list['CharacterFavorite']
+                      ] = relationship(back_populates='users')
+    favorites_planet: Mapped[list['PlanetFavorite']
+                             ] = relationship(back_populates='users')
+    favorite_vehicle: Mapped[list['VehicleFavorite']
+                             ] = relationship(back_populates='users')
+    favorite_starship: Mapped[list['StarshipFavorite']
+                              ] = relationship(back_populates='users')
+
+    def __repr__(self):
+        return f'Usuario {self.email}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
 
 class Character(db.Model):
-    __tablename__= 'people'
+    __tablename__ = 'people'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
     height: Mapped[int] = mapped_column(Integer)
@@ -26,23 +42,36 @@ class Character(db.Model):
     eyes_color: Mapped[str] = mapped_column(String(50))
     birth_day: Mapped[str] = mapped_column(String(20))
     mass: Mapped[int] = mapped_column(nullable=True)
-    favorites_by: Mapped[list['CharactereFavorite']] = relationship(back_populates='characteres')
+    favorites_by: Mapped[list['CharacterFavorite']
+                         ] = relationship(back_populates='character')
+    
+    def __repr__(self):
+        return f'Personaje {self.name}'
+
 class Planet(db.Model):
-    __tablename__= 'planets'
+    __tablename__ = 'planets'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(60), nullable=False)
     climate: Mapped[str] = mapped_column(String(120), nullable=True)
     diametre: Mapped[int] = mapped_column(nullable=True)
     population: Mapped[int] = mapped_column(nullable=True)
-    favorite_by: Mapped[list['PlanetFavorite']] = relationship(back_populates = 'planetas')
+    favorite_by: Mapped[list['PlanetFavorite']
+                        ] = relationship(back_populates='planetas')
+    
+    def __repr__(self):
+        return f'Planet {self.name}'
 
 class Vehicle(db.Model):
-    __tablename__= 'vehicles'
+    __tablename__ = 'vehicles'
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(120), nullable=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
     model: Mapped[str] = mapped_column(String(120))
     passengers: Mapped[int] = mapped_column(nullable=False)
-    favorite_by: Mapped[list['VehicleFavorite']] = relationship(back_populates = 'vehiculo')
+    favorite_by: Mapped[list['VehicleFavorite']
+                        ] = relationship(back_populates='vehiculo')
+    
+    def __repr__(self):
+        return f'{self.name}'
 
 class Starship(db.Model):
     __tablename__ = 'starships'
@@ -51,47 +80,57 @@ class Starship(db.Model):
     model: Mapped[str] = mapped_column(String(120))
     passengers: Mapped[int] = mapped_column(nullable=False)
     length: Mapped[int] = mapped_column(nullable=False)
+    favorite_by: Mapped[list['StarshipFavorite']
+                        ] = relationship(back_populates='starship')
 
-class CharactereFavorite(db.Model):
-    __tablename__ = 'charactere_favorite'
+    def __repr__(self):
+        return f'{self.name}'
+
+class CharacterFavorite(db.Model):
+    __tablename__ = 'character_favorite'
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int]= mapped_column(ForeignKey('usuario.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
     users: Mapped['User'] = relationship(back_populates='favorites')
 
-    character_id:Mapped[int] = mapped_column(ForeignKey('people.id'))
-    characteres: Mapped['Character'] = relationship(back_populates='favorites_by')
+    character_id: Mapped[int] = mapped_column(ForeignKey('people.id'))
+    character: Mapped['Character'] = relationship(
+        back_populates='favorites_by')
+
+    def __repr__(self):
+        return f'le gusta el id # {self.id}'
 
 class PlanetFavorite(db.Model):
     __tablename__ = 'planet_favorite'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
-    users: Mapped['User'] = relationship(back_populates = 'favorites_planet')
+    users: Mapped['User'] = relationship(back_populates='favorites_planet')
 
     planet_id: Mapped[int] = mapped_column(ForeignKey('planets.id'))
-    planetas: Mapped['Planet'] = relationship(back_populates = 'favorite_by')
+    planetas: Mapped['Planet'] = relationship(back_populates='favorite_by')
+
+    def __repr__(self):
+        return f'le gusta {self.users}'
 
 class VehicleFavorite(db.Model):
     __tablename__ = 'vehicle_favorite'
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id:Mapped[int] = mapped_column(ForeignKey('usuario.id'))
-    users: Mapped['User'] = relationship(back_populates = 'favorite_vehicle')
+    user_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
+    users: Mapped['User'] = relationship(back_populates='favorite_vehicle')
 
     vehicle_id: Mapped[int] = mapped_column(ForeignKey('vehicles.id'))
-    vehiculo: Mapped['Vehicle'] = relationship(back_populates = 'favorite_by')
+    vehiculo: Mapped['Vehicle'] = relationship(back_populates='favorite_by')
+    
+    def __repr__(self):
+        return f'le gusta {self.users}'
 
 class StarshipFavorite(db.Model):
     __tablename__ = 'starship_favorite'
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id:Mapped[int] = mapped_column(ForeignKey('usuario.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('usuario.id'))
+    users: Mapped['User'] = relationship(back_populates='favorite_starship')
 
     starship_id: Mapped[int] = mapped_column(ForeignKey('starships.id'))
-       
+    starship: Mapped['Starship'] = relationship(back_populates='favorite_by')
 
-
-
-def serialize(self):
-    return {
-        "id": self.id,
-        "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    def __repr__(self):
+        return f'le gusta el usuario con el id # {self.user_id}'
